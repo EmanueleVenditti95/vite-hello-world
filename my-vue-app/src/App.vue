ebf1787f8de3eb001f8f21ab756845eaab9232b1
EmanueleVenditti95 <vendittiema@gmail.com>
2023-11-03 14:55:22 +0100

    Add - print title

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="title">La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+.title {
+  color: red;
+  font-size: 80px;
+}
+</style>
